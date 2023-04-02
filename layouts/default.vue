1722cc054216a7d88b7f03baaf8584883f6b5706
Sergey Tikhomirov <tihomirov.pro@gmail.com>
2023-04-02 17:46:42 +0300

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,9 @@
 <script setup>
 onMounted(async () => {
-  initUser();
-});
-
-const active = useActiveExercise()
-
+  initUser()
+})
 </script>
 
 <template lang="pug">
-.wrapper
-    ExercisesList(v-if="!active")
-    
-    .info(v-if="active")
-      slot
-        
+slot
 </template>
-
-<style lang="stylus">
-.wrapper
-  display grid
-  grid-template-columns: 300px 1fr
-  height 100%
-  @media (max-width: 767px)
-    grid-template-columns: 1fr
-</style>
